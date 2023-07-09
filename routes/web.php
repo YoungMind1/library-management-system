2f4f47557e85fc9c0f7b116ddaef96a41c082bed
Amir Hossein <amir.1381.azhdarnzhad@outlook.com>
2023-07-09 23:25:17 +0330

    Remove unnecessary dashboard route

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
