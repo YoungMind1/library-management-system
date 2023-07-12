da1e1580ce3b020cbe39a2315bc301ee234ee796
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2023-07-12 12:06:18 +0330

    Add register and login links to navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -51,13 +51,12 @@
                 @endrole
             </div>
 
-            @auth
                 <!-- Settings Dropdown -->
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>{{ Auth::user()?->name }}</div>
 
                                 <div class="ml-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -68,6 +67,15 @@
                         </x-slot>
 
                         <x-slot name="content">
+                        @guest
+                            <x-dropdown-link :href="route('login')">
+                                {{ __('Login') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('register')">
+                                {{ __('Register') }}
+                            </x-dropdown-link>
+                        @endguest
+                        @auth
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
@@ -82,10 +90,10 @@
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
+                        @endauth
                         </x-slot>
                     </x-dropdown>
                 </div>
-            @endauth
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
