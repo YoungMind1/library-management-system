e84597e4a31cc0309c702bd96f398319536a1c43
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2023-07-12 10:44:32 +0330

    Create categories.edit page

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,2 +1,26 @@
+@extends('layouts.app')
 
-<?php
+@section('content')
+    <div class="max-w-2xl mx-auto px-4 py-8">
+        <h1 class="text-2xl font-bold mb-4">Edit Category</h1>
+        <form action="{{ route('admin.categories.update', $category->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
+            <div class="mb-4">
+                <label for="name" class="block text-gray-700 font-bold mb-2">Name</label>
+                <input type="name" name="name" id="name" class="form-input rounded-md shadow-sm border-gray-300" value="{{ old('name') ?? $category->name }}" required autofocus>
+                @error('name')
+                    <p class="text-red-500 mt-2">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="flex items-center justify-between">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Edit
+                </button>
+                <a href="{{ route('admin.categories.index') }}" class="text-gray-600 hover:text-gray-800">
+                    Cancel
+                </a>
+            </div>
+        </form>
+    </div>
+@endsection
