cb1b9b322492007aa17b6467dd1155ea3824891c
AmirHossein <amir.1381.azhdarnzhad@gmail.com>
2023-07-12 10:46:13 +0330

    Add paginate buttons and show links to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,21 +24,24 @@
                     </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-201">
-                    @foreach($books as $book)
-                        <tr>
-                            <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->id}}</td>
-                            <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->name}}</td>
-                            <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->ISBN}}</td>
-                            <td class="px-7 py-4 text-center whitespace-nowrap">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                 </svg>
-                            </td>
-                        </tr>
-                    @endforeach
+                        @foreach($books as $book)
+                            <tr>
+                                <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->id}}</td>
+                                <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->name}}</td>
+                                <td class="px-7 py-4 text-center whitespace-nowrap">{{$book->ISBN}}</td>
+                                <td class="px-7 py-4 text-center whitespace-nowrap">
+                                    <a class="inline-block" href="{{route('books.show', $book)}}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
+                {{$books->links()}}
             </div>
         </div>
     </div>
